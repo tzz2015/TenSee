9f224031be4e127b5762581f1db71077d66e9e7e
tzz2015 <1374329135@qq.com>
2019-10-14 23:02:18 +0800

    哈哈

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const eventBus = require('./utils/eventbus.js')
 App({
   globalData: {
-    // baseUrl: 'https://mini.test.link-nemo.com:6670/',
-    baseUrl: 'http://172.16.20.13:8000/',
+    baseUrl: 'https://mini.test.link-nemo.com:6670/',
+    // baseUrl: 'http://172.16.20.13:8000/',
     bus: eventBus.eventBus,
     userInfo: null,
     openId: null,
